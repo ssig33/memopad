6ef37954f304ba238f9912786b9ba012cd7415e7
ssig33 <mail@ssig33.com>
2017-10-23 16:02:31 +0900

    use production build

--- webpack.production.js
+++ webpack.production.js
@@ -19,5 +19,8 @@ module.exports = {
       }
     ],
   },
-  plugins: [new webpack.optimize.UglifyJsPlugin()]
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)})
+  ]
 }
